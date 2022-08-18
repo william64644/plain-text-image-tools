d10f5c6ddeec45e7e1f0d81d2f25f8a450596fc3
William <59394187+william64644@users.noreply.github.com>
2022-08-18 16:53:09 -0300

    matrix to pgm working

--- main.cpp
+++ main.cpp
@@ -9,25 +9,12 @@
 #include "print_vector.h"
 #include "print_matrix.h"
 #include "dpkg.h"
+#include "get_int_matrix_from_double_matrix.h"
+#include "repack_pgm.h"
 
 using namespace std;
 
-vector<vector<int>> get_int_matrix_from_double_matrix(vector<vector<double>> double_matrix)
-{
 
-	vector<vector<int>> int_matrix(double_matrix.size(), vector<int>(double_matrix[0].size()));
-	
-	for (int y = 0; y < double_matrix.size(); y ++)
-	{
-		for (int x = 0; x < double_matrix[y].size(); x ++)
-		{
-			double_matrix[x][y] = double_matrix[x][y] * 1000; // scale up matrix data
-			int_matrix[x][y] = static_cast<int> (double_matrix[x][y]);
-		}
-	}
-	
-	return int_matrix;
-}
 
 int main(){
 
@@ -40,10 +27,9 @@ int main(){
 */
 
 	vector<vector<double>> weights = dpkg("networks/untrained_Input_weights.txt");
-	vector<vector<int>> pgm_matrix = get_int_matrix_from_double_matrix(weights);
 	
-	print_matrix(pgm_matrix);
-
+	repack_pgm(weights, "file.pgm");
+	
     return 0;
 }
-//ssssssssssssssssssssssssssssssssss
+//sssssssssssssssssssssssssssssssssssss
--- write_pgm.h
+++ write_pgm.h
@@ -6,6 +6,25 @@
 
 using namespace std;
 
+int get_biggest_in_matrix(vector<vector<int>> data)
+{
+	int biggest = 0;
+	
+	for (unsigned int row = 0; row < data.size(); row++)
+	{
+        	for (unsigned int column = 0; column < data[row].size(); column++)
+        	{
+            		if (data[row][column] > biggest)
+            		{
+            			biggest = data[row][column];
+            		}
+		}
+    	}
+    	
+    	return biggest;
+	
+}
+
 void write_pgm(vector<vector<int>> data, string output_file)
 {
     vector<string> output(data.size());
@@ -16,8 +35,10 @@ void write_pgm(vector<vector<int>> data, string output_file)
             output[row] = output[row] + to_string(data[row][column]) + ' ';
         }
     }
+    
+    int brightest_color = get_biggest_in_matrix(data);
 
-    string header = "P2\n" + to_string(data.size()) + " " + to_string(data[0].size()) + "\n255";
+    string header = "P2\n" + to_string(data.size()) + " " + to_string(data[0].size()) + "\n" + to_string(brightest_color) + "\n";
 
     output.insert(output.begin(), header);
 
